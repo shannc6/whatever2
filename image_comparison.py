995739838073add9f6670fd5caf74dc4d3b378a9
Shannon Chen <shannc6@cs.washington.edu>
2021-01-12 13:30:18 -0800

    read image url and process

--- image_comparison.py
+++ image_comparison.py
@@ -11,20 +11,8 @@ import cv2
 from PIL import Image
 from io import BytesIO
 import urllib.request
-
+import validators
 import matplotlib.pyplot as plt
-
-
-# def mse(imageA, imageB):
-# 	# the 'Mean Squared Error' between the two images is the
-# 	# sum of the squared difference between the two images;
-# 	# NOTE: the two images must have the same dimension
-# 	err = np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
-# 	err /= float(imageA.shape[0] * imageA.shape[1])
-	
-# 	# return the MSE, the lower the error, the more "similar"
-# 	# the two images are
-# 	return err
 
 # METHOD #1: OpenCV, NumPy, and urllib
 def url_to_image(url):
@@ -36,24 +24,22 @@ def url_to_image(url):
     # return the image
     return image
 
+def isUrl(path):
+    valid = validators.url(path)
+    return valid
+
+def processUrl(url):
+    response = requests.get(url)
+    img = Image.open(BytesIO(response.content))
+    img = img.save('local.jpg')
+    imgRead = cv2.imread('local.jpg')
+    return imgRead
+
 def compare_images(imageAPath, imageBPath):
     # compute the mean squared error and structural similarity
-    imageA = cv2.imread(imageAPath)
-    imageB = cv2.imread(imageBPath)
+    imageA = processUrl(imageAPath) if isUrl(imageAPath) else cv2.imread(imageAPath)
+    imageB = processUrl(imageBPath) if isUrl(imageBPath) else cv2.imread(imageBPath)
     s = measure.compare_ssim(imageA, imageB, multichannel=True)
-    # setup the figure
-    #fig = plt.figure(title)
-    #plt.suptitle("SSIM: %.2f" % (s))
-    # show first image
-    #ax = fig.add_subplot(1, 2, 1)
-    #plt.imshow(imageA, cmap = plt.cm.gray)
-    #plt.axis("off")
-    # show the second image
-    #ax = fig.add_subplot(1, 2, 2)
-    #plt.imshow(imageB, cmap = plt.cm.gray)
-    #plt.axis("off")
-    # show the images
-    #plt.show()
     return s
 
 def compare_images2(imageAPath, imageBPath):
@@ -65,44 +51,10 @@ def compare_images2(imageAPath, imageBPath):
     plt.imshow(img)
     plt.show()
 
-def test():
-    # compute the mean squared error and structural similarity
-    #response = requests.get('https://images.app.goo.gl/ymq4LeVVYF5Pq5Bi7')
-    # This portion is part of my test code
-    response = requests.get('https://images.app.goo.gl/ymq4LeVVYF5Pq5Bi7')
-    #img = Image.open(BytesIO(response.content))
-    
-    byteImgIO = io.BytesIO()
-    byteImg = Image.open(response.content)
-    byteImg.save(byteImgIO, "PNG")
-    byteImg = byteImgIO.read()
-
-    dataBytesIO = io.BytesIO(byteImg)
-    Image.open(dataBytesIO)
-
-
-    # Non test code
-    dataBytesIO = io.BytesIO(byteImg)
-    Image.open(dataBytesIO)
-  
-    #s = measure.compare_ssim(img, img, multichannel=True)
-    #print(s)
-    #imageB = cv2.imread(imageBPath)
-    
-    # setup the figure
-    #fig = plt.figure(title)
-    #plt.suptitle("SSIM: %.2f" % (s))
-    # show first image
-    #ax = fig.add_subplot(1, 2, 1)
-    #plt.imshow(imageA, cmap = plt.cm.gray)
-    #plt.axis("off")
-    # show the second image
-    #ax = fig.add_subplot(1, 2, 2)
-    #plt.imshow(imageB, cmap = plt.cm.gray)
-    #plt.axis("off")
-    # show the images
-    #plt.show()
-    #return s
+def test(a):
+    if a == 1:
+        t = 0
+    print(t)
 
 if __name__ == "__main__":
     a = 0
@@ -112,4 +64,10 @@ if __name__ == "__main__":
     # imageC = cv2.imread("white.jpg")
     #imageA = cv2.cvtColor(imageA, cv2.COLOR_BGR2GRAY)  
     #imageB = imageA
-    compare_images2('https://images.app.goo.gl/ymq4LeVVYF5Pq5Bi7','https://images.app.goo.gl/ymq4LeVVYF5Pq5Bi7')
+    # compare_images2('https://images.app.goo.gl/ymq4LeVVYF5Pq5Bi7','https://images.app.goo.gl/ymq4LeVVYF5Pq5Bi7')
+    # compare_images('black.jpg', 'https://consequenceofsound.net/wp-content/uploads/2019/05/pikachu-e1557247424342.jpg?quality=80')
+    # s1 = compare_images('black.jpg', 'white.jpg')
+    s2 = compare_images('https://consequenceofsound.net/wp-content/uploads/2019/05/pikachu-e1557247424342.jpg?quality=80', 'https://consequenceofsound.net/wp-content/uploads/2019/05/pikachu-e1557247424342.jpg?quality=80')
+    # print(s1)
+    print(s2)
+    # test(1)
